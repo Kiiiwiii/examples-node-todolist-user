33bff7fbf9670fe728d8ac73158b8b0cd17f888d
Kiwi <317484048@qq.com>
2018-12-16 19:02:54 +0100

    append log file

--- scripts/log.ts
+++ scripts/log.ts
@@ -1,16 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
 import fs from 'fs';
 
-export default function(name: string) {
+export default function(path: string, name: string) {
   return function (req: Request, _res: Response, next: NextFunction) {
     const date = new Date().toString();
     const url = req.url;
     const method = req.method;
-    fs.appendFile(name, `${date}, use method: ${method}, access url: ${url}. \n`, (err) => {
-      if(err) {
-        console.log(err);
-      }
-    });
-    next();
+    function checkAndAppendFile() {
+      fs.appendFile(`${path}/${name}`, `${date}, use method: ${method}, access url: ${url}. \n`, (err) => {
+        if (err) {
+          fs.mkdir(`${path}`, { recursive: true }, (err) => {
+            if (err) throw err;
+            checkAndAppendFile();
+          });
+        }
+        next();
+      });
+    }
+    checkAndAppendFile();
   };
 }
