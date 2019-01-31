41b84fe9f8f703a1b050bbee89d6d120f87bf7a4
Kiwi <317484048@qq.com>
2019-01-31 14:45:07 +0100

    use require instead of dynamic ts import

--- scripts/config/config.ts
+++ scripts/config/config.ts
@@ -1,17 +1,13 @@
+// heroku will config(set) process.env.NODE_ENV to production
+const env = process.env.NODE_ENV || 'development';
 
-export async function startConfigProcess() {
-  // heroku will config(set) process.env.NODE_ENV to production
-  const env = process.env.NODE_ENV || 'development';
-
-  // all config of production env will be added in heroku
-  // all config of development env and test env will be added here
-  if (env === 'development' || env === 'test') {
-    const config = await import('./config.json');
-    process.env.NODE_ENV = env;
-    Object.keys((config as any)[env]).forEach(key => {
-      process.env[key] = (config as any)[env][key];
-    });
-    return true;
-  }
-  return true;
+// all config of production env will be added in heroku
+// all config of development env and test env will be added here
+if (env === 'development' || env === 'test') {
+  // tslint:disable-next-line:no-var-requires
+  const config = require('./config.json');
+  process.env.NODE_ENV = env;
+  Object.keys((config as any)[env]).forEach(key => {
+    process.env[key] = (config as any)[env][key];
+  });
 }
--- scripts/server.ts
+++ scripts/server.ts
@@ -1,40 +1,28 @@
-import {startConfigProcess} from './config/config';
+import express from 'express';
+import './config/config';
+import log from './log';
+import bodyParser from 'body-parser';
+import apiRouter from './router-middleware/api';
+import './db/mongodb-connect';
 
-startConfigProcess().then(() => {
-  async function runServer() {
-    const express = (await import('express')).default;
-    const log = (await import('./log')).default;
-    const bodyParser = (await import('body-parser')).default;
-    const apiRouter = (await import('./router-middleware/api')).default;
-    await import('./db/mongodb-connect');
+const app = express();
+const port = process.env.PORT || 3000;
+// app level middleware
+app.use(log(__dirname + '/../log', 'app-log.txt'));
 
-    const app = express();
-    const port = process.env.PORT || 3000;
-    // app level middleware
-    app.use(log(__dirname + '/../log', 'app-log.txt'));
+// static file
+app.use('/static', express.static(__dirname + '/../client/public'));
 
-    // static file
-    app.use('/static', express.static(__dirname + '/../client/public'));
+// router middleware - api data
+app.use(bodyParser.json());
+app.use('/api', apiRouter);
 
-    // router middleware - api data
-    app.use(bodyParser.json());
-    app.use('/api', apiRouter);
-
-    // root - single page application - entry point
-    app.get('*', (req: any, res: any) => {
-      res.sendFile('index.html', {
-        root: __dirname + '/../client',
-      });
-    });
-
-    // tslint:disable-next-line:no-console
-    app.listen(port, () => console.log(`http://zhan.com:${port}`));
-  }
-  runServer();
+// root - single page application - entry point
+app.get('*', (_req, res) => {
+  res.sendFile('index.html', {
+    root: __dirname + '/../client',
+  });
 });
 
-// import express from 'express';
-// import log from './log';
-// import bodyParser from 'body-parser';
-// import apiRouter from './router-middleware/api';
-// import './db/mongodb-connect';
+// tslint:disable-next-line:no-console
+app.listen(port, () => console.log(`http://zhan.com:${port}`));
